4699eb1a90d2eee46b362c0c783138a4eebb0266
Jean Pérez Peralta <108840864+JeanGoP@users.noreply.github.com>
2024-12-15 00:50:33 -0500

    BAse de datos

--- install_msodbcsql.sh
+++ install_msodbcsql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Agrega el repositorio de Microsoft
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+# Actualiza e instala
 apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
